d08134ed1a04de34d9ef6b6721e44801ad18fabb
leandrosineriz <leandrosineriz@gmail.com>
2017-11-15 07:03:56 -0300

    TP4
    NavegadorWeb

---
+++ TP4/Archivos/Texto.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        private string _archivo;
+
+        public Texto(string archivo)
+        {
+            this._archivo = archivo;
+        }
+
+        /// <summary>
+        /// Guarda "datos" en el archivo
+        /// </summary>
+        /// <param name="datos"> Lo que se va a guardar en el archivo </param>
+        /// <returns> true si no hubo errores </returns>
+        public bool guardar(string datos)
+        {
+            bool retorno = true;
+            try
+            {
+                StreamWriter scritor = new StreamWriter(this._archivo, true);
+
+                scritor.WriteLine(datos);
+
+                scritor.Close();
+            }
+            catch (Exception)
+            {
+
+                retorno = false;
+            }
+
+
+
+            return retorno;
+        }
+
+
+        /// <summary>
+        /// Lee todos los datos del archivo y los retorna en una lista
+        /// </summary>
+        /// <param name="datos"> Una lista de string sin inicializar </param>
+        /// <returns> true si no hubo errores </returns>
+        public bool leer(out List<string> datos)
+        {
+            bool retorno = true;
+            datos = new List<string>();
+
+            try
+            {
+                StreamReader lectora = new StreamReader(this._archivo);
+
+                while (!lectora.EndOfStream)
+                {
+
+                    datos.Add(lectora.ReadLine());
+                }
+
+                lectora.Close();
+
+            }
+            catch (Exception)
+            {
+
+                retorno = false;
+            }
+
+            return retorno;
+        }
+
+    }
+}
---
+++ TP4/Navegador/frmWebBrowser.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using Hilo;
+using System.IO;
+
+namespace Navegador
+{
+    public partial class frmWebBrowser : Form
+    {
+        private const string ESCRIBA_AQUI = "Escriba aquí...";
+        Archivos.Texto archivos;
+
+        public frmWebBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void frmWebBrowser_Load(object sender, EventArgs e)
+        {
+            this.txtUrl.SelectionStart = 0;  //This keeps the text
+            this.txtUrl.SelectionLength = 0; //from being highlighted
+            this.txtUrl.ForeColor = Color.Gray;
+            this.txtUrl.Text = frmWebBrowser.ESCRIBA_AQUI;
+
+            archivos = new Archivos.Texto(frmHistorial.ARCHIVO_HISTORIAL);
+        }
+
+        #region "Escriba aquí..."
+        private void txtUrl_MouseMove(object sender, MouseEventArgs e)
+        {
+            Cursor.Current = Cursors.IBeam; //Without this the mouse pointer shows busy
+        }
+
+        private void txtUrl_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (this.txtUrl.Text.Equals(frmWebBrowser.ESCRIBA_AQUI) == true)
+            {
+                this.txtUrl.Text = "";
+                this.txtUrl.ForeColor = Color.Black;
+            }
+            if (e.KeyValue == 13)
+            {
+                this.btnIr_Click(sender, e);
+            }
+        }
+
+        private void txtUrl_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (this.txtUrl.Text.Equals(null) == true || this.txtUrl.Text.Equals("") == true)
+            {
+                this.txtUrl.Text = frmWebBrowser.ESCRIBA_AQUI;
+                this.txtUrl.ForeColor = Color.Gray;
+            }
+        }
+
+        private void txtUrl_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.txtUrl.SelectAll();
+        }
+        #endregion
+
+        /// <summary>
+        /// Modifica la barra de descarga.
+        /// </summary>
+        /// <param name="progreso"></param>
+        private void ProgresoDescarga(int progreso)
+        {
+            if (statusStrip.InvokeRequired)
+            {
+                ProgresoDescargaCallback d = new ProgresoDescargaCallback(ProgresoDescarga);
+                this.Invoke(d, new object[] { progreso });
+            }
+            else
+            {
+                tspbProgreso.Value = progreso;
+            }
+        }
+
+        /// <summary>
+        /// Muestra en el visor el Html descargado
+        /// </summary>
+        /// <param name="html"></param>
+        private void FinDescarga(string html)
+        {
+            if (rtxtHtmlCode.InvokeRequired)
+            {
+                FinDescargaCallback d = new FinDescargaCallback(FinDescarga);
+                this.Invoke(d, new object[] { html });
+            }
+            else
+            {
+                rtxtHtmlCode.Text = html;
+            }
+        }
+
+        /// <summary>
+        /// Inicia la descarga de la pagina en un hilo secundario
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnIr_Click(object sender, EventArgs e)
+        {
+            Thread hilo = new Thread(IniciarDescarga);
+
+            hilo.Start();
+        }
+
+        private void mostrarTodoElHistorialToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmHistorial frm = new frmHistorial();
+
+            frm.ShowDialog();
+        }
+
+        /// <summary>
+        /// Metodo que descarga la pagina y guarda su direccion en el historial
+        /// </summary>
+        private void IniciarDescarga()
+        {
+            Uri url = new Uri("https://" + this.txtUrl.Text);
+            Descargador d = new Descargador(url);
+            d.EventFinDescarga += FinDescarga;
+            d.EventProgresoDescarga += ProgresoDescarga;
+            d.IniciarDescarga();
+
+            archivos.guardar("https://" + this.txtUrl.Text);
+
+        }
+
+    }
+}
