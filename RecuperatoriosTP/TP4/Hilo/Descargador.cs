953430b3227363d67ed565a16c7304d8493baf86
leandrosineriz <leandrosineriz@gmail.com>
2017-11-23 11:53:19 -0300

    Recuperatorio TP4
    Correccion del error de las paginas http

---
+++ RecuperatoriosTP/TP4/Hilo/Descargador.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public delegate void FinDescargaCallback(string html);
+    public delegate void ProgresoDescargaCallback(int progreso);
+
+    public class Descargador
+    {
+        private Uri direccion;
+
+        public Descargador(Uri direccion)
+        {
+            this.direccion = direccion;
+        }
+
+        /// <summary>
+        /// Inicia la descarga del Html
+        /// </summary>
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += WebClientDownloadCompleted;
+
+
+                cliente.DownloadStringAsync(direccion);
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public event FinDescargaCallback EventFinDescarga;
+
+        public event ProgresoDescargaCallback EventProgresoDescarga;
+
+        /// <summary>
+        /// Lanza un evento con el progreso de la descarga como parametro
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            EventProgresoDescarga(e.ProgressPercentage);
+
+        }
+
+        /// <summary>
+        /// Lanza un evento con el resultado de la descarga como parametro
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            EventFinDescarga(e.Result);
+        }
+    }
+}
---
+++ RecuperatoriosTP/TP4/Navegador/frmHistorial.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Navegador
+{
+    public partial class frmHistorial : Form
+    {
+        public const string ARCHIVO_HISTORIAL = "historico.dat";
+
+        public frmHistorial()
+        {
+            InitializeComponent();
+        }
+
+        private void frmHistorial_Load(object sender, EventArgs e)
+        {
+            this.CargarHistorial();
+        }
+
+        /// <summary>
+        /// Carga los datos del historial en el visor del form.
+        /// </summary>
+        private void CargarHistorial()
+        {
+            Archivos.Texto archivos = new Archivos.Texto(ARCHIVO_HISTORIAL);
+
+            List<string> datos;
+
+            if (archivos.leer(out datos))
+            {
+                foreach (string i in datos)
+                {
+                    this.lstHistorial.Items.Add(i);
+                }
+            }
+
+
+        }
+
+    }
+}
